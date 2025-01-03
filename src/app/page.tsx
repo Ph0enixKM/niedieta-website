1794a3516e19c112c3c5e97d175ae42224a33612
Phoenix Himself <pkaras.it@gmail.com>
2025-01-03 15:54:02 +0000

    fix: black weeks banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     return (
 		<div className="container">
 			<Jumbotron />
-			{/* <BlackWeekBanner /> */}
+			<BlackWeekBanner />
 			<Store />
 		</div>
     );
--- src/components/BlackWeekBanner/BlackWeekBanner.tsx
+++ src/components/BlackWeekBanner/BlackWeekBanner.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import styles from './BlackWeekBanner.module.css';
 
 export default function BlackWeekBanner() {
