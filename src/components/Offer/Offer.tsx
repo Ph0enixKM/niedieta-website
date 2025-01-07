52930e056aaf7602d49652345e0e11f870f547e3
Phoenix Himself <pkaras.it@gmail.com>
2025-01-07 10:15:30 +0000

    feat: offer

--- src/components/Offer/Offer.tsx
+++ src/components/Offer/Offer.tsx
@@ -7,13 +7,38 @@ export default function Offer() {
             title="Jadłospis"
             price={100}
             color="#fc3376"
-            details={[]}
+            details={[
+                'rozpoczęcie współpracy w ciągu 5 dni roboczych (w tym czasie powstanie Twoje konto w aplikacji, poprzez którą możliwy będzie szybki kontakt, monitorowanie postępów i wypełnienie wstępnej ankiety)',
+                'indywidualny jadłospis 7-dniowy ułożony na podstawie ankiety (dostosowany do Twoich potrzeb i upodobań,  jadłospis otrzymasz w 7 dni roboczych od wypełnienia ankiety)',
+                'brak nielubianych produktów',
+                'wymiana posiłków w aplikacji',
+                'interaktywna lista zakupów',
+                'proponowana suplementacja',
+                'kontakt i odpowiedzi na wiadomości w 48h przez aplikację',
+                'analiza postępów i wsparcie',
+                'płatność jednorazowa'
+            ]}
         />
         <OfferItem
             title="Dieta+"
             price={150}
             color="#eb4c44"
-            details={[]}
+            details={[
+                '60 minut rozmowy online',
+                'Wywiad zdrowotny i żywieniowy',
+                'Analiza aktualnego sposobu żywienia',
+                'Wspólne opracowanie strategii działania',
+                'Wsparcie i kontakt w aplikacji przez miesiąc od wizyty',
+                'rozpoczęcie współpracy w ciągu 3 dni roboczych (w tym czasie powstanie Twoje konto w aplikacji, poprzez którą możliwy będzie szybki kontakt, monitorowanie postępów i wypełnienie wstępnej ankiety)',
+                'indywidualny jadłospis 7-dniowy ułożony na podstawie ankiety (dostosowany do Twoich potrzeb i upodobań,  jadłospis otrzymasz w 7 dni roboczych od wypełnienia ankiety)',
+                'brak nielubianych produktów',
+                'wymiana posiłków w aplikacji',
+                'interaktywna lista zakupów',
+                'proponowana suplementacja',
+                'kontakt i odpowiedzi na wiadomości w 24h przez aplikację',
+                'analiza postępów i wsparcie',
+                'płatność jednorazowa'
+            ]}
             top
         />
         <OfferItem
