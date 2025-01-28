af2f3eb8863347c3f25963d8223e9d94956a2cc8
Phoenix Himself <pkaras.it@gmail.com>
2025-01-28 11:58:24 +0000

    feat: offer

--- src/components/Offer/Offer.tsx
+++ src/components/Offer/Offer.tsx
@@ -1,27 +1,32 @@
+'use client'
+
+import { useState } from "react"
 import OfferItem from "../OfferItem/OfferItem"
 import styles from "./Offer.module.css"
 
 export default function Offer() {
-    return <div className={styles.container}>
+    const [showFull, setShowFull] = useState(false);
+
+    return <div className={[styles.container, showFull && styles.full].filter(Boolean).join(' ')}>
         <OfferItem
             title="Jadłospis"
-            price={100}
+            price={150}
             color="#fc3376"
             details={[
-                'rozpoczęcie współpracy w ciągu 5 dni roboczych (w tym czasie powstanie Twoje konto w aplikacji, poprzez którą możliwy będzie szybki kontakt, monitorowanie postępów i wypełnienie wstępnej ankiety)',
+                'rozpoczęcie współpracy w ciągu **5 dni roboczych** (w tym czasie powstanie Twoje konto w aplikacji, poprzez którą możliwy będzie szybki kontakt, monitorowanie postępów i wypełnienie wstępnej ankiety)',
                 'indywidualny jadłospis 7-dniowy ułożony na podstawie ankiety (dostosowany do Twoich potrzeb i upodobań,  jadłospis otrzymasz w 7 dni roboczych od wypełnienia ankiety)',
                 'brak nielubianych produktów',
                 'wymiana posiłków w aplikacji',
                 'interaktywna lista zakupów',
                 'proponowana suplementacja',
-                'kontakt i odpowiedzi na wiadomości w 48h przez aplikację',
+                'kontakt i odpowiedzi na wiadomości w **48h** przez aplikację',
                 'analiza postępów i wsparcie',
                 'płatność jednorazowa'
             ]}
         />
         <OfferItem
             title="Dieta+"
-            price={150}
+            price={200}
             color="#eb4c44"
             details={[
                 '60 minut rozmowy online',
@@ -29,13 +34,13 @@ export default function Offer() {
                 'Analiza aktualnego sposobu żywienia',
                 'Wspólne opracowanie strategii działania',
                 'Wsparcie i kontakt w aplikacji przez miesiąc od wizyty',
-                'rozpoczęcie współpracy w ciągu 3 dni roboczych (w tym czasie powstanie Twoje konto w aplikacji, poprzez którą możliwy będzie szybki kontakt, monitorowanie postępów i wypełnienie wstępnej ankiety)',
+                'rozpoczęcie współpracy w ciągu **3 dni roboczych** (w tym czasie powstanie Twoje konto w aplikacji, poprzez którą możliwy będzie szybki kontakt, monitorowanie postępów i wypełnienie wstępnej ankiety)',
                 'indywidualny jadłospis 7-dniowy ułożony na podstawie ankiety (dostosowany do Twoich potrzeb i upodobań,  jadłospis otrzymasz w 7 dni roboczych od wypełnienia ankiety)',
                 'brak nielubianych produktów',
                 'wymiana posiłków w aplikacji',
                 'interaktywna lista zakupów',
                 'proponowana suplementacja',
-                'kontakt i odpowiedzi na wiadomości w 24h przez aplikację',
+                'kontakt i odpowiedzi na wiadomości w **24h** przez aplikację',
                 'analiza postępów i wsparcie',
                 'płatność jednorazowa'
             ]}
@@ -43,7 +48,7 @@ export default function Offer() {
         />
         <OfferItem
             title="Konsultacje"
-            price={100}
+            price={150}
             color="#ed6e40"
             details={[
                 '60 minut rozmowy online',
@@ -53,5 +58,6 @@ export default function Offer() {
                 'Wsparcie i kontakt w aplikacji przez miesiąc od wizyty'
             ]}
         />
+        <div className={styles['show-more']} onClick={() => setShowFull(true)} />
     </div>
 }
--- src/components/OfferItem/OfferItem.tsx
+++ src/components/OfferItem/OfferItem.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import { defaultMaxListeners } from "events"
 import styles from "./OfferItem.module.css"
+import { useState } from "react"
 
 interface Props {
     title: string,
@@ -9,12 +12,19 @@ interface Props {
     top?: boolean
 }
 
+const parseMarkdown = (text: string) => {
+    return text.replace(/\*\*([^\*]+)\*\*/, "<b>$1</b>")
+}
+
 export default function OfferItem({ title, price, color, top, details }: Props) {
-    return <div className={[styles.card, top && styles.top].filter(Boolean).join(' ')}>
+    const [showFull, setShowFull] = useState(false);
+
+    return <div className={[styles.card, top && styles.top, showFull && styles.full].filter(Boolean).join(' ')}>
         <div className={styles.title}>{title}</div>
         <div className={styles.price} style={{ color }}>{price} zł</div>
         <ul className={styles.list}>
-            {details.map((item) => <li>{item}</li>)}
+            {details.map((item) => <li dangerouslySetInnerHTML={{ __html: parseMarkdown(item) }}></li>)}
         </ul>
+        <div className={styles["show-more"]} onClick={() => setShowFull(true)} />
     </div>
 }
