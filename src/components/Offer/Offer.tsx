e0936e0f44010487ee3f334a948cf757cb94fc7a
Phoenix Himself <pkaras.it@gmail.com>
2025-01-28 14:02:05 +0100

    feat: offer

--- src/components/Offer/Offer.tsx
+++ src/components/Offer/Offer.tsx
@@ -7,57 +7,63 @@ import styles from "./Offer.module.css"
 export default function Offer() {
     const [showFull, setShowFull] = useState(false);
 
-    return <div className={[styles.container, showFull && styles.full].filter(Boolean).join(' ')}>
-        <OfferItem
-            title="Jadłospis"
-            price={150}
-            color="#fc3376"
-            details={[
-                'rozpoczęcie współpracy w ciągu **5 dni roboczych** (w tym czasie powstanie Twoje konto w aplikacji, poprzez którą możliwy będzie szybki kontakt, monitorowanie postępów i wypełnienie wstępnej ankiety)',
-                'indywidualny jadłospis 7-dniowy ułożony na podstawie ankiety (dostosowany do Twoich potrzeb i upodobań,  jadłospis otrzymasz w 7 dni roboczych od wypełnienia ankiety)',
-                'brak nielubianych produktów',
-                'wymiana posiłków w aplikacji',
-                'interaktywna lista zakupów',
-                'proponowana suplementacja',
-                'kontakt i odpowiedzi na wiadomości w **48h** przez aplikację',
-                'analiza postępów i wsparcie',
-                'płatność jednorazowa'
-            ]}
-        />
-        <OfferItem
-            title="Dieta+"
-            price={200}
-            color="#eb4c44"
-            details={[
-                '60 minut rozmowy online',
-                'Wywiad zdrowotny i żywieniowy',
-                'Analiza aktualnego sposobu żywienia',
-                'Wspólne opracowanie strategii działania',
-                'Wsparcie i kontakt w aplikacji przez miesiąc od wizyty',
-                'rozpoczęcie współpracy w ciągu **3 dni roboczych** (w tym czasie powstanie Twoje konto w aplikacji, poprzez którą możliwy będzie szybki kontakt, monitorowanie postępów i wypełnienie wstępnej ankiety)',
-                'indywidualny jadłospis 7-dniowy ułożony na podstawie ankiety (dostosowany do Twoich potrzeb i upodobań,  jadłospis otrzymasz w 7 dni roboczych od wypełnienia ankiety)',
-                'brak nielubianych produktów',
-                'wymiana posiłków w aplikacji',
-                'interaktywna lista zakupów',
-                'proponowana suplementacja',
-                'kontakt i odpowiedzi na wiadomości w **24h** przez aplikację',
-                'analiza postępów i wsparcie',
-                'płatność jednorazowa'
-            ]}
-            top
-        />
-        <OfferItem
-            title="Konsultacje"
-            price={150}
-            color="#ed6e40"
-            details={[
-                '60 minut rozmowy online',
-                'Wywiad zdrowotny i żywieniowy',
-                'Analiza aktualnego sposobu żywienia',
-                'Wspólne opracowanie strategii działania',
-                'Wsparcie i kontakt w aplikacji przez miesiąc od wizyty'
-            ]}
-        />
-        <div className={styles['show-more']} onClick={() => setShowFull(true)} />
+    return <div className={styles.outer}>
+        <div className={styles.title}>Nasza Oferta</div>
+        <div className={[styles.container, showFull && styles.full].filter(Boolean).join(' ')}>
+            <OfferItem
+                title="Jadłospis"
+                price={150}
+                color="#fc3376"
+                details={[
+                    'rozpoczęcie współpracy w ciągu **5 dni roboczych** (w tym czasie powstanie Twoje konto w aplikacji, poprzez którą możliwy będzie szybki kontakt, monitorowanie postępów i wypełnienie wstępnej ankiety)',
+                    'indywidualny jadłospis 7-dniowy ułożony na podstawie ankiety (dostosowany do Twoich potrzeb i upodobań,  jadłospis otrzymasz w 7 dni roboczych od wypełnienia ankiety)',
+                    'brak nielubianych produktów',
+                    'wymiana posiłków w aplikacji',
+                    'interaktywna lista zakupów',
+                    'proponowana suplementacja',
+                    'kontakt i odpowiedzi na wiadomości w **48h** przez aplikację',
+                    'analiza postępów i wsparcie',
+                    'płatność jednorazowa'
+                ]}
+                href="https://www.naffy.io/niedieta/jadlospis-ywS"
+            />
+            <OfferItem
+                title="Dieta+"
+                price={200}
+                color="#eb4c44"
+                details={[
+                    '60 minut rozmowy online',
+                    'Wywiad zdrowotny i żywieniowy',
+                    'Analiza aktualnego sposobu żywienia',
+                    'Wspólne opracowanie strategii działania',
+                    'Wsparcie i kontakt w aplikacji przez miesiąc od wizyty',
+                    'rozpoczęcie współpracy w ciągu **3 dni roboczych** (w tym czasie powstanie Twoje konto w aplikacji, poprzez którą możliwy będzie szybki kontakt, monitorowanie postępów i wypełnienie wstępnej ankiety)',
+                    'indywidualny jadłospis 7-dniowy ułożony na podstawie ankiety (dostosowany do Twoich potrzeb i upodobań,  jadłospis otrzymasz w 7 dni roboczych od wypełnienia ankiety)',
+                    'brak nielubianych produktów',
+                    'wymiana posiłków w aplikacji',
+                    'interaktywna lista zakupów',
+                    'proponowana suplementacja',
+                    'kontakt i odpowiedzi na wiadomości w **24h** przez aplikację',
+                    'analiza postępów i wsparcie',
+                    'płatność jednorazowa'
+                ]}
+                href="https://www.naffy.io/niedieta/jadlospis-ub3"
+                top
+            />
+            <OfferItem
+                title="Konsultacje"
+                price={150}
+                color="#ed6e40"
+                details={[
+                    '60 minut rozmowy online',
+                    'Wywiad zdrowotny i żywieniowy',
+                    'Analiza aktualnego sposobu żywienia',
+                    'Wspólne opracowanie strategii działania',
+                    'Wsparcie i kontakt w aplikacji przez miesiąc od wizyty'
+                ]}
+                href="https://www.naffy.io/niedieta/konsultacja-online-KeA"
+            />
+            <div className={styles['show-more']} onClick={() => setShowFull(true)} />
+        </div>
     </div>
 }
--- src/components/OfferItem/OfferItem.tsx
+++ src/components/OfferItem/OfferItem.tsx
@@ -9,6 +9,7 @@ interface Props {
     price: number,
     details: string[],
     color?: string,
+    href?: string,
     top?: boolean
 }
 
@@ -16,15 +17,18 @@ const parseMarkdown = (text: string) => {
     return text.replace(/\*\*([^\*]+)\*\*/, "<b>$1</b>")
 }
 
-export default function OfferItem({ title, price, color, top, details }: Props) {
+export default function OfferItem({ title, price, color, top, details, href }: Props) {
     const [showFull, setShowFull] = useState(false);
 
     return <div className={[styles.card, top && styles.top, showFull && styles.full].filter(Boolean).join(' ')}>
         <div className={styles.title}>{title}</div>
         <div className={styles.price} style={{ color }}>{price} zł</div>
         <ul className={styles.list}>
-            {details.map((item) => <li dangerouslySetInnerHTML={{ __html: parseMarkdown(item) }}></li>)}
+            {details.map((item, index) => <li key={index} dangerouslySetInnerHTML={{ __html: parseMarkdown(item) }}></li>)}
         </ul>
+        <a className={styles.button} href={href}>
+            Zobacz Ofertę
+        </a>
         <div className={styles["show-more"]} onClick={() => setShowFull(true)} />
     </div>
 }
